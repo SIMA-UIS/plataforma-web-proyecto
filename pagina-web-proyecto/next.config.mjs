104c5ea42ff199be167dd0bc758158025d623801
Levir Hernandez <levir_h@hotmail.com>
2025-09-08 23:22:35 -0500

    hotfix/state

--- pagina-web-proyecto/next.config.mjs
+++ pagina-web-proyecto/next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true, // Skip TS type errors
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Skip ESLint errors
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
-
