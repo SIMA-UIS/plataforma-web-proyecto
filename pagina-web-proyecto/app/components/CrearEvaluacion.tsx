698d19e7da7d26a4edc0687d49793a23502b98e4
Levir Hernandez <levir_h@hotmail.com>
2025-09-12 01:08:40 -0500

    version estable

--- pagina-web-proyecto/app/components/CrearEvaluacion.tsx
+++ pagina-web-proyecto/app/components/CrearEvaluacion.tsx
@@ -305,9 +305,8 @@ export default function CrearEvaluacion({
           </div>
 
           {/* Botonera */}
-          <div className="flex justify-center gap-4 mt-6">
-            {/* Contenedor para Agregar pregunta */}
-            <div className="relative">
+          <div className="flex justify-center gap-4 mt-6 relative">
+            <div className="flex flex-col sm:flex-row gap-4 items-center">
               <button
                 onClick={() => setShowTypePicker((s) => !s)}
                 className="inline-flex items-center gap-2 px-4 py-2 border-2 border-primary-40 text-primary-40 bg-white 
@@ -318,7 +317,7 @@ export default function CrearEvaluacion({
               </button>
 
               {showTypePicker && (
-                <div className="absolute left-0 mt-2 w-64 bg-white border border-gray-300 rounded-lg shadow p-2">
+                <div className="absolute z-10 mt-2 w-64 bg-white border border-gray-300 rounded-lg shadow p-2">
                   <p className="text-sm font-medium px-2 py-1 text-gray-700">Tipo de pregunta</p>
                   <div className="flex flex-col">
                     <button onClick={() => addQuestionOfType("OPEN")} className="text-left px-3 py-2 hover:bg-gray-100 rounded">Respuesta abierta</button>
@@ -328,19 +327,17 @@ export default function CrearEvaluacion({
                   </div>
                 </div>
               )}
+
+              <button
+                onClick={omitEvaluation}
+                className="inline-flex items-center gap-2 px-4 py-2 border-2 border-gray-500 text-white bg-gray-400 
+      rounded-xl font-medium shadow-sm hover:bg-gray-600 hover:text-white 
+      active:scale-95 transition"
+              >
+                Omitir evaluación
+              </button>
             </div>
-
-            {/* Botón Omitir evaluación */}
-            <button
-              onClick={omitEvaluation}
-              className="inline-flex items-center gap-2 px-4 py-2 border-2 border-gray-500 text-white bg-gray-400 
-    rounded-xl font-medium shadow-sm hover:bg-gray-600 hover:text-white 
-    active:scale-95 transition"
-            >
-              Omitir evaluación
-            </button>
           </div>
-
         </>
       )}
     </div>
